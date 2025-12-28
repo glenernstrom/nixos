db019bd2cea1e791c6ce22bfc3a6dde2e80be495
Glen Ernstrom <otterbrain@protonmail.com>
2025-12-28 01:59:08 -0500

    added gnome settigns

---
+++ home/gnome.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  # Display server + GNOME
+  services.xserver.enable = true;
+
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # GNOME needs dconf
+  programs.dconf.enable = true;
+
+  # GNOME keyring (needed for Git, browsers, Wi-Fi, etc.)
+  services.gnome.gnome-keyring.enable = true;
+
+  # Portals (Wayland + Flatpak compatibility, even if you don’t use Flatpak)
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-gnome
+  ];
+
+  # Light GNOME utilities (system-level)
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+    gnome-extension-manager
+  ];
+}
+
