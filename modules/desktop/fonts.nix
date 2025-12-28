8c1c5bed9a7b6ab2c2ca8ccb1f79a065ddf3bfff
Glen Ernstrom <otterbrain@protonmail.com>
2025-12-28 02:50:30 -0500

    emoji fonts package name fix

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -8,7 +8,7 @@
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       liberation_ttf
       dejavu_fonts
       fira-code
