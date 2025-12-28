3ebbe88321351ef80198b9bbb195285d7d69eef2
Glen Ernstrom <otterbrain@protonmail.com>
2025-12-28 02:12:35 -0500

    typo fix

--- home/gnome.nix
+++ home/gnome.nix
@@ -1,20 +1,6 @@
-{ pkgs, ... }:
+{pkgs, ... }:
 
 {
-  # Display server + GNOME
-  services.xserver.enable = true;
-
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  # GNOME keyring (needed for Git, browsers, Wi-Fi, etc.)
-  services.gnome.gnome-keyring.enable = true;
-
-  # Portals (Wayland + Flatpak compatibility, even if you don’t use Flatpak)
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gnome
-  ];
 
   # Light GNOME utilities (system-level)
   home.packages = with pkgs; [
