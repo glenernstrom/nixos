b827b8ff4da424295c6f43309d0d9d82bc5fd6f2
Glen Ernstrom <otterbrain@protonmail.com>
2025-12-28 19:11:07 -0500

    added reminder distrobox aliases

---
+++ home/shell.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bash.enable = true;
+
+  programs.bash.shellAliases = {
+    rstudio = "echo \"Use: distrobox-enter -n science-r -- rstudio\"";
+    fiji    = "echo \"Use: distrobox-enter -n fiji -- fiji\"";
+  };
+
+  
+  programs.fish.enable = true:
+
+  programs.fish.shellAliases = {
+    rstudio = "echo \"Use: distrobox-enter -n science-r -- rstudio\"";
+    fiji    = "echo \"Use: distrobox-enter -n fiji -- fiji\"";
+  };
+}
+
+
