c053ef9a0e11d3adfdbb9addf63327f6fb61dcdb
Glen Ernstrom <otterbrain@protonmail.com>
2025-12-29 14:50:14 -0500

    missed a bracket

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -1,11 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
-xdg.configFile."ghostty/config" = {
-  text = ''
-    theme = catppuccin-mocha
-    font-family = JetBrains Mono
-    font-size = 14
-  '';
-  force = true;
-};
+{
+  xdg.configFile."ghostty/config" = {
+    text = ''
+      theme = catppuccin-mocha
+      font-family = JetBrains Mono
+      font-size = 12
+    '';
+    force = true;
+  };
+}
 
