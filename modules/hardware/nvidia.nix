27d840dd654a26d4c37ac55eb0eaed50ce4f9e7e
Glen Ernstrom <otterbrain@protonmail.com>
2025-12-29 14:41:20 -0500

    added script to help with suspend issue

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -26,5 +26,48 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
+
+  # https://discourse.nixos.org/t/black-screen-after-suspend-hibernate-with-nvidia/54341/6
+  # https://discourse.nixos.org/t/suspend-problem/54033/28
+    systemd = {
+      # Uncertain if this is still required or not.
+      services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+
+      services."gnome-suspend" = {
+        description = "suspend gnome shell";
+        before = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+          "nvidia-suspend.service"
+          "nvidia-hibernate.service"
+        ];
+        wantedBy = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+        ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
+        };
+      };
+      services."gnome-resume" = {
+        description = "resume gnome shell";
+        after = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+          "nvidia-resume.service"
+        ];
+        wantedBy = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+        ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
+        };
+      };
+    };
+
+
 }
 
