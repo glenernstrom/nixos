314aa94f8124deb007bc09d36bf29c488044a0aa
Glen Ernstrom <otterbrain@protonmail.com>
2026-03-17 13:54:20 -0400

    move to flatpak apps

--- modules/desktop/gaming.nix
+++ modules/desktop/gaming.nix
@@ -13,10 +13,6 @@
 
   # --- Gaming/QoL tools ---
   environment.systemPackages = with pkgs; [
-    mangohud
-    goverlay
-    lutris
-    heroic
     mesa-demos     # provides glxinfo
     vulkan-tools   # useful for debugging Vulkan/driver stack
   ];
