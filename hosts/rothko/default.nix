e75e500600a1045b29d79bb915e6e5c341417867
Glen Ernstrom <otterbrain@protonmail.com>
2026-03-31 17:04:52 -0400

    some kernel parity between desktop pc and laptop

--- hosts/rothko/default.nix
+++ hosts/rothko/default.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ pkgs, lib, config ... }:
 
-{
+{ 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "rothko";
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -9,9 +11,23 @@
   programs.fuse.userAllowOther = true;
 
 
+  programs.deconf.profiles.user.dataases = [
+   {
+     settings = {
+      "org/gnome/mutter" = {
+        experimental-features = [
+          "scale-monitor-framebuffer"
+          "variable-refresh-rate"
+          "xwayland-native-scaling"
+          "autoclose-xwayland"
+          ];
+        };
+      };
+    }
+  ];
+  
   imports = [
     ./hardware-configuration.nix
-
     ../../modules/hardware/cpu-intel.nix
   ];
 }
